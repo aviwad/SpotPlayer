5ce3ad511f94b2ac7ffe19d1f83c7dd58e127a28
Avi Wadhwa <aviwad@gmail.com>
2022-04-15 03:20:06 -0700

    start xcode project

---
+++ SpotPlayer/SpotPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotPlayerApp.swift
+//  SpotPlayer
+//
+//  Created by Avi Wadhwa on 2022-04-15.
+//
+
+import SwiftUI
+
+@main
+struct SpotPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
